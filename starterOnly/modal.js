fe09e6400be8ccf88a42892d7cf6ee713965a14b
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2025-06-13 15:55:05 +0200

    Resolves #8- Fixed: cut the wrong image that caused console error + rename X in tabsWrapper

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,9 +1,9 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var tabsWrapper = document.getElementById("myTopnav");
+  if (tabsWrapper.className === "topnav") {
+    tabsWrapper.className += " responsive";
   } else {
-    x.className = "topnav";
+    tabsWrapper.className = "topnav";
   }
 }
 
